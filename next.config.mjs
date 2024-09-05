564560abc588fc370263cb0b4081375ab5534708
Aman Jain <amanbanzal282003@gmail.com>
2024-09-05 20:16:35 +0530

    org 2 changed in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ export default withSentryConfig(withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "abs",
+org: "self-hyq",
 project: "care-pulse",
 
 // Only print logs for uploading source maps in CI
